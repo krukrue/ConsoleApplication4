f0606f4c8336bb8f1f091db76589a947cbaf2b08
krukrue <92872517+krukrue@users.noreply.github.com>
2022-10-30 20:26:12 +0100

    +Comments

--- ConsoleApplication4/CountingSort.h
+++ ConsoleApplication4/CountingSort.h
@@ -12,47 +12,67 @@ public:
 	vector<T> sum;
 	int max;
 
-	void InitVector(vector<T> vect) { // initializing our vectors
-		intVector = vect;
-		counter.clear();             // clear vector 
-		max = *max_element(vect.begin(), vect.end()); 
-		for (int i = 0; i < max + 1; i++) {
-			counter.push_back(0);      // init for DistributionCountingSort
-		}
+	void InitVector(vector<T> vect); // initializing our vectors
+
+	vector<int> DistributionCountingSort(vector<int> temp);
+	void printResult(vector<T> sum);  // display function
+
+	vector<T> ComparisonCountingSort(vector<T> temp);
+};
+
+//Slozitost Algoritmu = O(n)
+template<typename T>
+void CountingSort<T>::InitVector(vector<T> vect)
+{
+	intVector = vect;
+	counter.clear();             // clear vector 
+	max = *max_element(vect.begin(), vect.end());
+	for (int i = 0; i < max + 1; i++) {
+		counter.push_back(0);      // init for DistributionCountingSort
+	}
+}
+
+//Slozitost Algoritmu = O(n)
+inline vector<int> CountingSort<int>::DistributionCountingSort(vector<int> temp)
+{
+	InitVector(temp);
+	vector<int> sum(intVector.size());
+
+	for (int i = 0; i < intVector.size(); i++) { // compure frequience
+		int j = intVector[i];
+		counter[j] = counter[j] + 1;
+	}
+	for (int i = 1; i < max + 1; i++) { // reuse for distribution
+		counter[i] = counter[i] + counter[i - 1];
 	}
 
-	vector<int> DistributionCountingSort(vector<int> temp) {
-		InitVector(temp);
-		vector<int> sum(intVector.size());
 
-		for (int i = 0; i < intVector.size(); i++) { // compure frequience
-			int j = intVector[i];
-			counter[j] = counter[j] + 1;
-		}
-		for (int i = 1; i < max + 1; i++) { // reuse for distribution
-			counter[i] = counter[i] + counter[i - 1];
-		}
-
-
-		for (int i = intVector.size() - 1; i > -1; i--) { // compare and final result
-			int j = intVector[i];
-			counter[j] = counter[j] - 1;
-			sum[counter[j]] = intVector[i];
-		}
-
-
-		printResult(sum);
-		
-		return sum;
-	}
-	void printResult(vector<T> sum) { // display function
-		cout << "Sorted list: " << endl;
-		for (int i = 0; i < sum.size(); i++) {
-			cout << sum[i] << endl;
-		}
+	for (int i = intVector.size() - 1; i > -1; i--) { // compare and final result
+		int j = intVector[i];
+		counter[j] = counter[j] - 1;
+		sum[counter[j]] = intVector[i];
 	}
 
-	vector<T> ComparisonCountingSort(vector<T> temp) {
+
+	printResult(sum);
+
+	return sum;
+}
+
+template<typename T>
+inline void CountingSort<T>::printResult(vector<T> sum) 
+{
+	cout << "Sorted list: " << endl;
+	for (int i = 0; i < sum.size(); i++) {
+		cout << sum[i] << endl;
+	}
+}
+
+//Slozitost Algoritmu = O(n^2)
+template<typename T>
+inline vector<T> CountingSort<T>::ComparisonCountingSort(vector<T> temp)
+{
+	{
 		InitVector(temp);
 		vector<int> count;
 		vector<T> sum(intVector.size());
@@ -65,7 +85,7 @@ public:
 				if (intVector[i] < intVector[j]) {
 					count[j] = count[j] + 1;
 
-				} 
+				}
 				else {
 					count[i] = count[i] + 1;
 				}
@@ -77,6 +97,6 @@ public:
 		}
 		printResult(sum);
 
-		return sum;	
+		return sum;
 	}
-};
+}
